d8fcbbd5c45449c6dcf68c7addea9e790288df59
ismaacab <mario.ismael.canevari.09@gmail.com>
2026-07-02 09:05:24 -0300

    Delete E18.py

--- E18.py
+++
@@ -1,2 +0,0 @@
-#E18.py)aplicar un filtro de desenfoque gaussiano a una imagen. Mostrar la imagen original y la filtrada. Hacer la convolución manual desde la
-#celda (1,1) hasta la (n-1, n-1), Usar  el kernel 
